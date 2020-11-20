7a559b6dc171fbab4b9de075c621a8452a5894c7
manumn2610 <67653169+manumn2610@users.noreply.github.com>
2020-11-20 17:53:01 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld
 
 public static void main(String args[])
 {
-System.out.println("*****HelloWorld Welcome to Jenkins");
+System.out.println("*****HelloWorld Welcome to Jenkins  CHANGED to trigger the build");
 }
 
 }
